a3721cc453ad1e085158e5ae8ad4ad0f71f0a7b7
digital-how <hello@digital-how.com>
2026-01-14 15:39:58 +0100

    Fix GTM implementation: remove invalid head tag, place script in body

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
+      <body className={`${cormorant.variable} ${inter.variable} ${montserrat.variable} font-sans antialiased`}>
         {/* Google Tag Manager */}
         <Script id="google-tag-manager" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -61,8 +61,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-MNR8RB7Z');`}
         </Script>
-      </head>
-      <body className={`${cormorant.variable} ${inter.variable} ${montserrat.variable} font-sans antialiased`}>
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
